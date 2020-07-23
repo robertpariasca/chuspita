727254989787a1ace4ca9c955cfb8beb88903e1e
Efra <robertpariasca34@gmail.com>
2020-07-22 21:10:46 -0500

    Formulario select completos y SP guardad

--- src/scripts-init/combos_llenar.js
+++ src/scripts-init/combos_llenar.js
@@ -54,8 +54,83 @@ $(document).ready(() => {
 
 $('#segmanempresa').change(function() {
     $("#segmanempresa option[value='000']").remove();
-    var val = $("#segmanempresa option:selected").text();
-    alert(val);
+    $("#segmansede").empty();
+    var o = new Option("", "000");
+    $(o).html("");
+    $("#segmansede").append(o);
+    
+    $("#segmancargo").empty();
+    var o = new Option("", "000");
+    $(o).html("");
+    $("#segmancargo").append(o);
+    //$(this).children("option[value='000']").remove();
+    var ItemJSON={
+        "idempresa": $(this).children("option:selected").val()
+    };
+    //alert($(this).children("option:selected").val());
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+            var respuesta = JSON.parse(this.responseText);
+            //alert(respuesta);
+            //alert(this.responseText);
+           //$("#segmansede").empty();
+            for(i = 0; i < respuesta.length; i++){
+
+            var o = new Option(respuesta[i].desoficina, respuesta[i].idoficina);
+          $(o).html(respuesta[i].desoficina);
+          $("#segmansede").append(o);
+
+        }
+
+        }
+   };
+   xhttp.open("POST", "http://localhost/ChuspitaApi/controller/seguridad.cargaropcionesoficina.controller.php", true);
+   xhttp.setRequestHeader("Content-type", "application/json");
+   xhttp.send(JSON.stringify(ItemJSON));
+    //var val = $("#segmanempresa option:selected").text();
+    //alert(val);
+});
+
+$('#segmansede').change(function() {
+    $("#segmansede option[value='000']").remove();
+    $("#segmancargo").empty();
+    var o = new Option("", "000");
+    $(o).html("");
+    $("#segmancargo").append(o);
+    
+    var ItemJSON={
+        "idempresa": $("#segmanempresa option:selected").val(),
+        "idoficina": $(this).children("option:selected").val()
+    };
+    //alert($(this).children("option:selected").val());
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+            var respuesta = JSON.parse(this.responseText);
+
+            //$("#segmancargo").empty();
+            for(i = 0; i < respuesta.length; i++){
+
+            var o = new Option(respuesta[i].descripcion, respuesta[i].idcargo);
+          $(o).html(respuesta[i].descripcion);
+          $("#segmancargo").append(o);
+
+        }
+
+        }
+   };
+   xhttp.open("POST", "http://localhost/ChuspitaApi/controller/seguridad.cargaropcionescargo.controller.php", true);
+   xhttp.setRequestHeader("Content-type", "application/json");
+   xhttp.send(JSON.stringify(ItemJSON));
+    //var val = $("#segmanempresa option:selected").text();
+    //alert(val);
 });
 
     });
+
+    
